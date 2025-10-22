2d9acdb36f771dcf2d4cf299a0a29a8eb05e221d
henrique expedito <henriqueexpedito964@gmail.com>
2025-10-22 11:30:58 -0300

    erro das imagens e meta tags corrigido

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: 'src',
-   server: {
+  server: {
     open: true,
     port: 4099,
     host: true,
     strictPort: true,
+  },
+  build: {
+    outDir: 'dist',
   },
 })
